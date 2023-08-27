a4ae8cdbaa78a075a8f98c5bc3fa1f6b9fe3f1a9
decagon <samuel.okafor@decagon.dev>
2023-08-27 15:50:22 +0100

    Update

--- src/main/java/com/example/newspringsecurity/Model/User.java
+++ src/main/java/com/example/newspringsecurity/Model/User.java
@@ -1,9 +1,6 @@
 package com.example.newspringsecurity.Model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,6 +10,7 @@ import org.hibernate.annotations.NaturalId;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Table(name = "Users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
