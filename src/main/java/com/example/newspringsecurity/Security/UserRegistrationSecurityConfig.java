fae1378e1317558be8d5be9e065f15d877b884bb
decagon <samuel.okafor@decagon.dev>
2023-08-27 16:14:11 +0100

    Update

--- src/main/java/com/example/newspringsecurity/Security/UserRegistrationSecurityConfig.java
+++ src/main/java/com/example/newspringsecurity/Security/UserRegistrationSecurityConfig.java
@@ -2,9 +2,11 @@ package com.example.newspringsecurity.Security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -12,5 +14,24 @@ public class UserRegistrationSecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .cors()
+                .and()
+                .csrf()
+                .disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/api/v1/register")
+                .permitAll()
+                .and()
+                .authorizeHttpRequests()
+                .requestMatchers("")
+                .hasAnyRole("USER", "ADMIN")
+                .and()
+                .formLogin()
+                .and().build();
     }
 }
