2652a7da6eb789a9001bb4b2d6d8646deee7c5e4
decagon <samuel.okafor@decagon.dev>
2023-08-27 14:40:07 +0100

    Update

--- src/main/java/com/example/newspringsecurity/Model/VerificationToken.java
+++ src/main/java/com/example/newspringsecurity/Model/VerificationToken.java
@@ -1,4 +1,44 @@
 package com.example.newspringsecurity.Model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Calendar;
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class VerificationToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String token;
+    private Date expirationTime;
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    private static final int EXPIRATION_TIME = 15;
+
+    public VerificationToken(String token, User user) {
+        this.token = token;
+        this.user = user;
+        this.expirationTime = this.getTokenExpirationTime();
+    }
+
+    public VerificationToken(String token) {
+        super();
+        this.token = token;
+        this.expirationTime = this.getTokenExpirationTime();
+    }
+
+    public Date getTokenExpirationTime() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(new Date().getTime());
+        calendar.add(Calendar.MINUTE, EXPIRATION_TIME);
+        return new Date(calendar.getTime().getTime());
+    }
 }
