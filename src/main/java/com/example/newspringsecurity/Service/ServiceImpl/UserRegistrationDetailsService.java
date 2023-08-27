c1381cef822e53f99eafd28b7f1931590bb38506
decagon <samuel.okafor@decagon.dev>
2023-08-27 16:01:53 +0100

    Update

---
+++ src/main/java/com/example/newspringsecurity/Service/ServiceImpl/UserRegistrationDetailsService.java
@@ -0,0 +1,21 @@
+package com.example.newspringsecurity.Service.ServiceImpl;
+
+import com.example.newspringsecurity.Repository.UserRepository;
+import com.example.newspringsecurity.Security.UserRegistrationDetails;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserRegistrationDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .map(UserRegistrationDetails::new)
+                .orElseThrow(()-> new UsernameNotFoundException("User not found"));
+    }
+}
